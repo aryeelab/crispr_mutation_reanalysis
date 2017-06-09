186c37b225ce44dde8509c477c9eb3ffe502e4c5
caleblareau <caleblareau@g.harvard.edu>
2017-06-09 00:52:52 -0400

    added strelka output

---
+++ lareau_bash/strelka.sh
@@ -0,0 +1,11 @@
+module load strelka/default
+
+/apps/source/strelka/1.0.11/strelka_workflow-1.0.11/bin/configureStrelkaWorkflow.pl --normal=fastq/SRR5450998.bwa.dedup.sorted.rg.bam --tumor=fastq/SRR5450996.bwa.dedup.sorted.rg.bam --ref=/data/aryee/pub/genomes/mm10/mm10.fa --config=config.ini --output-dir=./strelka_n98_t96
+/apps/source/strelka/1.0.11/strelka_workflow-1.0.11/bin/configureStrelkaWorkflow.pl --normal=fastq/SRR5450998.bwa.dedup.sorted.rg.bam --tumor=fastq/SRR5450997.bwa.dedup.sorted.rg.bam --ref=/data/aryee/pub/genomes/mm10/mm10.fa --config=config.ini --output-dir=./strelka_n98_t97
+
+/apps/source/strelka/1.0.11/strelka_workflow-1.0.11/bin/configureStrelkaWorkflow.pl --normal=fastq/SRR5450997.bwa.dedup.sorted.rg.bam --tumor=fastq/SRR5450996.bwa.dedup.sorted.rg.bam --ref=/data/aryee/pub/genomes/mm10/mm10.fa --config=config.ini --output-dir=./strelka_n97_t96
+/apps/source/strelka/1.0.11/strelka_workflow-1.0.11/bin/configureStrelkaWorkflow.pl --normal=fastq/SRR5450997.bwa.dedup.sorted.rg.bam --tumor=fastq/SRR5450998.bwa.dedup.sorted.rg.bam --ref=/data/aryee/pub/genomes/mm10/mm10.fa --config=config.ini --output-dir=./strelka_n97_t98
+
+/apps/source/strelka/1.0.11/strelka_workflow-1.0.11/bin/configureStrelkaWorkflow.pl --normal=fastq/SRR5450996.bwa.dedup.sorted.rg.bam --tumor=fastq/SRR5450997.bwa.dedup.sorted.rg.bam --ref=/data/aryee/pub/genomes/mm10/mm10.fa --config=config.ini --output-dir=./strelka_n96_t97
+/apps/source/strelka/1.0.11/strelka_workflow-1.0.11/bin/configureStrelkaWorkflow.pl --normal=fastq/SRR5450996.bwa.dedup.sorted.rg.bam --tumor=fastq/SRR5450998.bwa.dedup.sorted.rg.bam --ref=/data/aryee/pub/genomes/mm10/mm10.fa --config=config.ini --output-dir=./strelka_n96_t98
+
