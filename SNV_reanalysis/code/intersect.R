6522d27d3caa774a1e0fe5616c75484e55b1905e
caleblareau <caleblareau@g.harvard.edu>
2017-06-14 14:42:35 -0400

    overlap analysis

--- SNV_reanalysis/code/intersect.R
+++ SNV_reanalysis/code/intersect.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(GenomicRanges)
 library(data.table)
+library(diffloop)
 
 # 96 F05 Treated
 # 97 F03 Treated
@@ -31,9 +32,41 @@ readLofreq <- function(sample){
   gr
 }
 
-sample <- "n98_t96"
+intersectAll <- function(sample){
+  s <- readStrelka(sample)
+  m <- readMutect(sample)
+  l <- readLofreq(sample)
+  ov1 <- findOverlaps(s,m)
+  ovall <- findOverlaps(s[data.frame(ov1)$queryHits], l)
+  variantsCalledg <- l[data.frame(ovall)$subjectHits]
+  variantsCalledg 
+}
 
-s <- readStrelka(sample)
-m <- readMutect(sample)
-l <- readLofreq(sample)
+F03_FVB <- intersectAll("n98_t97")
+F05_FVB <- intersectAll("n98_t96")
+
+length(intersectAll("n97_t98"))
+
+# Read in the published variant data
+pubRead <- function(sample2){
+  ldf <- read.table(paste0("../original_variant_calls/",sample2,".snv.lofreq.v2.1.3a.filtered.vcf"))
+  lg <- makeGRangesFromDataFrame(ldf, seqnames.field = "V1", start.field = "V2", end.field = "V2")
+  mdf <- read.table(paste0("../original_variant_calls/", sample2, ".snv.mutect.v1.1.7.filtered.vcf"))
+  mg <- makeGRangesFromDataFrame(mdf, seqnames.field = "V1", start.field = "V2", end.field = "V2")
+  sdf <- read.table(paste0("../original_variant_calls/", sample2, ".snv.strelka.v1.0.14.filtered.vcf"))
+  sg <- makeGRangesFromDataFrame(sdf, seqnames.field = "V1", start.field = "V2", end.field = "V2")
+  ov1 <- findOverlaps(sg,mg)
+  ovall <- findOverlaps(sg[data.frame(ov1)$queryHits], lg)
+  variantsCalledg <- lg[data.frame(ovall)$subjectHits]
+  addchr(variantsCalledg)
+}
+
+F03_FVB_pub <- pubRead("F03--FVB")
+F05_FVB_pub <- pubRead("F05--FVB")
+
+# Quantify how many variants I was able to get
+sum(countOverlaps(F03_FVB, F03_FVB_pub))/length(F03_FVB_pub)
+sum(countOverlaps(F05_FVB, F05_FVB_pub))/length(F05_FVB_pub)
+
+sum(countOverlaps(F03_FVB_pub, F05_FVB_pub))
 
