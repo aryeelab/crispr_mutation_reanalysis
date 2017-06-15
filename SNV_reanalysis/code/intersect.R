8c14ec0af447a19444aee5779f3d9ea8923f1ee2
caleblareau <caleblareau@g.harvard.edu>
2017-06-15 00:29:02 -0400

    fixed intersect.R out convention

--- SNV_reanalysis/code/intersect.R
+++ SNV_reanalysis/code/intersect.R
@@ -50,7 +50,7 @@ flip_F05_FVB <- intersectAll("n96_t98")
 
 write.table(data.frame(F03_FVB)[,c(1,2,3)], file = "../new_processed_calls/F03_FVB_n98_t97.processed.bed", quote = FALSE, row.names = FALSE, col.names = FALSE)
 write.table(data.frame(F05_FVB)[,c(1,2,3)], file = "../new_processed_calls/F05_FVB_n98_t96.processed.bed", quote = FALSE, row.names = FALSE, col.names = FALSE)
-write.table(data.frame(flip_F03_FVB)[,c(1,2,3)], file = "../new_processed_calls/flip_FVB_F05_n97_t98.processed.bed", quote = FALSE, row.names = FALSE, col.names = FALSE)
+write.table(data.frame(flip_F03_FVB)[,c(1,2,3)], file = "../new_processed_calls/flip_FVB_F03_n97_t98.processed.bed", quote = FALSE, row.names = FALSE, col.names = FALSE)
 write.table(data.frame(flip_F05_FVB)[,c(1,2,3)], file = "../new_processed_calls/flip_FVB_F05_n96_t98.processed.bed", quote = FALSE, row.names = FALSE, col.names = FALSE)
 
 
