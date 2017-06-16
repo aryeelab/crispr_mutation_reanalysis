da75499b4d3d9f5c9164cb74cec7b336d5df9589
caleblareau <caleblareau@g.harvard.edu>
2017-06-16 16:57:20 -0400

    added common variant list

--- plots/commonVariantAnalysis.R
+++ plots/commonVariantAnalysis.R
@@ -20,6 +20,7 @@ countsdiff <- data.frame(
   c98 = unname(sapply(diff$gt98, function(d){sum(as.numeric(strsplit(d, split = "/")[[1]]))}))
 ) %>% data.matrix()
 
+
 # Compute the mode and then subtract it out
 # https://stackoverflow.com/questions/28094468/how-to-calculate-the-mode-of-all-rows-or-columns-from-a-dataframe
 modefunc <- function(x){
@@ -32,6 +33,14 @@ modefunc <- function(x){
 # I'm taking the mode to identify the "rare" individual and the contorting it such that
 # two 1s appear in the final matrix that indicate that both shared these mutations
 distingmat <- abs(abs(countsdiff - apply(countsdiff, 1, modefunc)) - 1)
+
+# Make bedfile output
+uniqVarMat <- abs(countsdiff - apply(countsdiff, 1, modefunc))
+uniqVarVec <- uniqVarMat[,1]*1 +  uniqVarMat[,2]*2 +  uniqVarMat[,3]*3
+odf <- df[(gdf$gt96 != gdf$gt97) | (gdf$gt96 != gdf$gt98) | (gdf$gt97 != gdf$gt98),c(1,2)]
+odf$pos2 <- odf$pos + 1
+odf$anno <- ifelse(uniqVarVec == 1, "F05", ifelse(uniqVarVec == 2, "F03", "FVB"))
+write.table(odf, file = "../SNV_reanalysis/snps/commonVariants.txt", sep = "\t", row.names = FALSE, col.names = FALSE, quote = FALSE)
 
 heatmap.2(t(distingmat), labRow=c("F05", "F03", "FVB"), labCol = NULL, trace="none",
           col = c("grey", "dodgerblue"), key = FALSE)
