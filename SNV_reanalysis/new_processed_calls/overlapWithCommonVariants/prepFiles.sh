7b0978eb35416fe04783b0cee7410f3777288cde
kclem <kclem@users.noreply.github.com>
2017-06-16 21:48:28 -0400

    Create prepFiles.sh

---
+++ SNV_reanalysis/new_processed_calls/overlapWithCommonVariants/prepFiles.sh
@@ -0,0 +1,16 @@
+#commonvariants.txt are SNPs from dbSNP that are different in one mouse. The fourth column is the mouse that has the 'other' genome. So entries with 'FVB' in the fourth column are the same in F03 and F05 and different in FVB.
+
+#use bedtools to find the closest common variant snp
+#-d flag prints the distance to the closest snp
+bedtools closest -a COMMON_F03_F05.bed -d -b commonVariants.txt > COMMON_F03_F05.bed.closest
+
+bedtools closest -a F05_FVB_n98_t96.processed_fixed.bed -d -b commonVariants.txt > F05_FVB_n98_t96.processed_fixed.bed.closest
+
+bedtools closest -a F03_FVB_n98_t97.processed_fixed.bed -d -b commonVariants.txt > F03_FVB_n98_t97.processed_fixed.bed.closest
+
+#create files with only F03 or only F05 SNPs
+bedtools intersect -v -a F03_FVB_n98_t97.processed_fixed.bed -b F05_FVB_n98_t96.processed_fixed.bed > F03_unique.bed
+bedtools closest -a F03_unique.bed -d -b commonVariants.txt > F03_unique.bed.closest
+
+bedtools intersect -v -a F05_FVB_n98_t96.processed_fixed.bed -b F03_FVB_n98_t97.processed_fixed.bed > F05_unique.bed
+bedtools closest -a F05_unique.bed -d -b commonVariants.txt > F05_unique.bed.closest
