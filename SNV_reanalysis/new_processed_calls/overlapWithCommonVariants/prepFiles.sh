6bbd0eb1a6ecf9aee6b443cf886aee356eee61ae
kclem <kclem@users.noreply.github.com>
2017-06-16 21:56:41 -0400

    Update with file locations

--- SNV_reanalysis/new_processed_calls/overlapWithCommonVariants/prepFiles.sh
+++ SNV_reanalysis/new_processed_calls/overlapWithCommonVariants/prepFiles.sh
@@ -1,4 +1,6 @@
 #commonvariants.txt are SNPs from dbSNP that are different in one mouse. The fourth column is the mouse that has the 'other' genome. So entries with 'FVB' in the fourth column are the same in F03 and F05 and different in FVB.
+#commonVariants.txt is here: https://github.com/aryeelab/crispr_reanalysis/blob/master/SNV_reanalysis/snps/commonVariants.txt
+#The other files (COMMON_F03_F05.bed, F05_FVB_n98_t96.processed_fixed.bed, F03_FVB_n98_t97.processed_fixed.bed) are here: https://github.com/aryeelab/crispr_reanalysis/blob/master/SNV_reanalysis/new_processed_calls/
 
 #use bedtools to find the closest common variant snp
 #-d flag prints the distance to the closest snp
