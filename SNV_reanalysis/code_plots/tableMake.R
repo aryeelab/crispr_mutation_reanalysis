a21d4cd52fe63d0c1d66183d8ed47f1743de4880
caleblareau <caleblareau@g.harvard.edu>
2017-06-24 15:46:58 -0400

    fixed sorting

--- SNV_reanalysis/code_plots/tableMake.R
+++ SNV_reanalysis/code_plots/tableMake.R
@@ -10,14 +10,20 @@ source("importData.R")
 # 98 FVB Control
 # n = normal; t = tumor
 
-F03_FVB <- data.frame(sort(intersectAll("n98_t97"))); F03_FVB$end <- F03_FVB$end + 1
-F05_FVB <- data.frame(sort(intersectAll("n98_t96"))); F05_FVB$end <- F05_FVB$end + 1
+gr <- intersectAll("n98_t97"); seqlevels(gr) <- sort(seqlevels(gr))
+F03_FVB <- data.frame(sort(gr)); F03_FVB$end <- F03_FVB$end + 1
+gr <- intersectAll("n98_t96"); seqlevels(gr) <- sort(seqlevels(gr))
+F05_FVB <- data.frame(sort(gr)); F05_FVB$end <- F05_FVB$end + 1
 
-FVB_F03 <- data.frame(sort(intersectAll("n97_t98"))); FVB_F03$end <- FVB_F03$end + 1
-F05_F03 <- data.frame(sort(intersectAll("n97_t96"))); F05_F03$end <- F05_F03$end + 1
+gr <- intersectAll("n97_t98"); seqlevels(gr) <- sort(seqlevels(gr))
+FVB_F03 <- data.frame(sort(gr)); FVB_F03$end <- FVB_F03$end + 1
+gr <- intersectAll("n97_t96")
+F05_F03 <- data.frame(sort(gr)); F05_F03$end <- F05_F03$end + 1
 
-FVB_F05 <- data.frame(sort(intersectAll("n96_t98"))); FVB_F05$end <- FVB_F05$end + 1
-F03_F05 <- data.frame(sort(intersectAll("n96_t97"))); F03_F05$end <- F03_F05$end + 1
+gr <- intersectAll("n96_t98"); seqlevels(gr) <- sort(seqlevels(gr))
+FVB_F05 <- data.frame(sort(gr)); FVB_F05$end <- FVB_F05$end + 1
+gr <- intersectAll("n96_t97");  seqlevels(gr) <- sort(seqlevels(gr))
+F03_F05 <- data.frame(sort(gr)); F03_F05$end <- F03_F05$end + 1
 
 
 # Export tables of intersected variants
@@ -32,15 +38,15 @@ write.table(F03_F05[,c(1,2,3,6)], file = "../new_processed_calls/tF03_nF05.bed",
 
 # Get the intersected variants
 
-F03_FVBg <- intersectAll("n98_t97")
-F05_FVBg <- intersectAll("n98_t96")
+F03_FVBg <- intersectAll("n98_t97"); seqlevels(F03_FVBg) <- sort(seqlevels(F03_FVBg))
+F05_FVBg <- intersectAll("n98_t96"); seqlevels(F05_FVBg) <- sort(seqlevels(F05_FVBg))
 ovg <- data.frame(sort(F03_FVBg[queryHits(findOverlaps(F03_FVBg,F05_FVBg))])); ovg$end <-  ovg$end + 1
 write.table(ovg[,c(1,2,3,6)], file = "../new_processed_calls/overlap_tF03tF05_nFVB.bed",
             quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
 
-FVB_F03g <- intersectAll("n97_t98")
-FVB_F05g <- intersectAll("n96_t98")
-ovg <- data.frame(sort(F03_FVBg[queryHits(findOverlaps(FVB_F03g,FVB_F05g))])); ovg$end <-  ovg$end + 1
+FVB_F03g <- intersectAll("n97_t98"); seqlevels(FVB_F03g) <- sort(seqlevels(FVB_F03g))
+FVB_F05g <- intersectAll("n96_t98"); seqlevels(FVB_F05g) <- sort(seqlevels(FVB_F05g))
+ovg <- data.frame(sort(FVB_F03g[queryHits(findOverlaps(FVB_F03g,FVB_F05g))])); ovg$end <-  ovg$end + 1
 write.table(ovg[,c(1,2,3,6)], file = "../new_processed_calls/overlap_tFVB_nF03nF05.bed",
             quote = FALSE, row.names = FALSE, col.names = FALSE, sep = "\t")
 
