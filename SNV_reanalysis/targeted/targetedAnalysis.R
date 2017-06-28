646d08108474ed78f1ac67a21b984d08cfcd9c78
caleblareau <caleblareau@g.harvard.edu>
2017-06-28 09:09:26 -0400

    targeted reanalysis

--- SNV_reanalysis/targeted/targetedAnalysis.R
+++ SNV_reanalysis/targeted/targetedAnalysis.R
@@ -1,108 +1,17 @@
-library(tidyverse)
-library(GenomicRanges)
-library(data.table)
-library(diffloop)
+source("targetedImport.R")
+options(warn=-1)
+length(intersectAll("F05", "novel"))
+length(intersectAll("F05", "dbSNP"))
+
+length(intersectAll("F03", "novel"))
+length(intersectAll("F03", "dbSNP"))
+
+expected <- bedToGRanges("GATK.out.anno.F03F05.bed")
+
+sum(countOverlaps(intersectLM("F03", "novel"),expected))
+sum(countOverlaps(intersectLM("F05", "novel"),expected))
+
+sum(countOverlaps(intersectLM("F03", "novel"), intersectLM("F05", "novel")))
+sum(countOverlaps(intersectLM("F03", "dbSNP"), intersectLM("F05", "dbSNP")))
 
 
-readStrelka <- function(sample){
-  file <- paste0("../new_variant_calls_raw/strelka_out/strelka_", sample, "/results/passed.somatic.snvs.vcf")
-  df <- read.table(file, sep = "\t")
-  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "V1", start.field = "V2", end.field = "V2")
-  gr
-}
-  
-readMutect <- function(sample, filtDBSNP = TRUE){
-  file <- paste0("../new_variant_calls_raw/mutect_out/mutect_", sample, "/", sample, "mutect.res.tsv")
-  df <- read.table(file, sep = "\t", header = TRUE)
-  if(filtDBSNP) df <- df[df$dbsnp_site == "NOVEL", ]
-  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "contig", start.field = "position", end.field = "position")
-  gr
-}
-
-readLofreq <- function(sample){
-  file <- paste0("../new_variant_calls_raw/lofreq_out/", sample, "somatic_final_minus-dbsnp.snvs.vcf.gz")
-  df <- data.frame(fread(paste("zcat < ", file)))
-  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "X.CHROM", start.field = "POS", end.field = "POS")
-  gr
-}
-
-intersectAll <- function(sample){
-  s <- readStrelka(sample)
-  m <- readMutect(sample)
-  l <- readLofreq(sample)
-  ov1 <- findOverlaps(s,m)
-  ovall <- findOverlaps(s[data.frame(ov1)$queryHits], l)
-  variantsCalledg <- l[data.frame(ovall)$subjectHits]
-  variantsCalledg 
-}
-
-#######################################
-# Now look at the downsampled analysis
-#######################################
-
-readStrelkaDS <- function(sample){
-  file <- paste0("../downsampled_variant_calls/strelka_out/strelka_ds_", sample, "/results/passed.somatic.snvs.vcf")
-  df <- read.table(file, sep = "\t")
-  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "V1", start.field = "V2", end.field = "V2")
-  gr
-}
-  
-readMutectDS <- function(sample, filtDBSNP = TRUE){
-  file <- paste0("../downsampled_variant_calls/mutect_out/", sample, ".downsampled.mutect.res.tsv")
-  df <- read.table(file, sep = "\t", header = TRUE)
-  if(filtDBSNP) df <- df[df$dbsnp_site == "NOVEL", ]
-  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "contig", start.field = "position", end.field = "position")
-  gr
-}
-
-readLofreqDS <- function(sample){
-  file <- paste0("../downsampled_variant_calls/lofreq_out/", sample, "somatic_final_minus-dbsnp.snvs.vcf.gz")
-  df <- data.frame(fread(paste("zcat < ", file)))
-  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "X.CHROM", start.field = "POS", end.field = "POS")
-  gr
-}
-
-intersectAllDS <- function(sample){
-  s <- readStrelkaDS(sample)
-  m <- readMutectDS(sample)
-  l <- readLofreqDS(sample)
-  ov1 <- findOverlaps(s,m)
-  ovall <- findOverlaps(s[data.frame(ov1)$queryHits], l)
-  variantsCalledg <- l[data.frame(ovall)$subjectHits]
-  variantsCalledg 
-}
-
-
-# Read in the published variant data
-
-readLofreqPub <- function(sample2){
-  ldf <- read.table(paste0("../original_variant_calls/",sample2,".snv.lofreq.v2.1.3a.filtered.vcf"))
-  lg <- makeGRangesFromDataFrame(ldf, seqnames.field = "V1", start.field = "V2", end.field = "V2")
-  lg
-}
-
-readMutectPub <- function(sample2){
-  mdf <- read.table(paste0("../original_variant_calls/", sample2, ".snv.mutect.v1.1.7.filtered.vcf"))
-  mg <- makeGRangesFromDataFrame(mdf, seqnames.field = "V1", start.field = "V2", end.field = "V2")
-  mg
-}
-
-readStrelkaPub <- function(sample2){
-  sdf <- read.table(paste0("../original_variant_calls/", sample2, ".snv.strelka.v1.0.14.filtered.vcf"))
-  sg <- makeGRangesFromDataFrame(sdf, seqnames.field = "V1", start.field = "V2", end.field = "V2")
-  sg
-}
-
-pubIntersectAll <- function(sample2){
-  lg <- readLofreqPub(sample2)
-  mg <- readMutectPub(sample2)
-  sg <- readStrelkaPub(sample2)
-  ov1 <- findOverlaps(sg,mg)
-  ovall <- findOverlaps(sg[data.frame(ov1)$queryHits], lg)
-  variantsCalledg <- lg[data.frame(ovall)$subjectHits]
-  addchr(variantsCalledg)
-}
-
-in2 <- function(gr1, gr2){
-  return(length(data.frame(findOverlaps(gr1, gr2))$queryHits))
-}
---
+++ SNV_reanalysis/targeted/targetedImport.R
@@ -0,0 +1,45 @@
+library(tidyverse)
+library(GenomicRanges)
+library(data.table)
+library(diffloop)
+
+
+readStrelka <- function(sample, type = "novel"){
+  file <- paste0("data/", sample, "_FVB_strelka.somatic.snvs.vcf.gz")
+  df <- read.table(file, sep = "\t")
+  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "V1", start.field = "V2", end.field = "V2")
+  gr
+}
+  
+readMutect <- function(sample, type = "novel", filt = TRUE){
+  file <- paste0("data/mutect_", sample, "_", type, ".vcf.gz")
+  df <- data.frame(fread(paste("zcat < ", file)))
+  if(filt) df <- df[df$judgement == "KEEP", ]
+  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "contig", start.field = "position", end.field = "position")
+  gr
+}
+
+readLofreq <- function(sample, type = "novel"){
+  file <- paste0("data/", sample, "_FVB-", type, "tumor_relaxed.vcf.gz")
+  df <- data.frame(fread(paste("zcat < ", file)))
+  gr <- makeGRangesFromDataFrame(df, keep.extra.columns = TRUE, seqnames.field = "X.CHROM", start.field = "POS", end.field = "POS")
+  gr
+}
+
+intersectAll <- function(sample, type = "novel"){
+  s <- readStrelka(sample, type)
+  m <- readMutect(sample, type)
+  l <- readLofreq(sample, type)
+  expected <- bedToGRanges(paste0("data/venn.", type, "110.bed"))
+  ov1 <- findOverlaps(s,m)
+  ov2 <- findOverlaps(s[data.frame(ov1)$queryHits], l)
+  g <- l[data.frame(ov2)$subjectHits]
+  ov3 <- findOverlaps(g,expected)
+  variantsCalledg <- g[data.frame(ov3)$queryHits]
+  variantsCalledg 
+}
+
+
+in2 <- function(gr1, gr2){
+  return(length(data.frame(findOverlaps(gr1, gr2))$queryHits))
+}
